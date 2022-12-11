dca08fd0d6f8df6835e9f6efddcd98b28cde1a30
khhandrea <khhandrea@naver.com>
2022-12-11 11:15:32 +0900

    2022.12.11 upload

--- 02504_괄호의 값.py
+++ 02504_괄호의 값.py
@@ -1,22 +1,33 @@
 buf = input()
 stack = []
 result = 0
+temp = 1
 
-for char in buf:
-    if char == '(' or char == '[':
+for idx, char in enumerate(buf):
+    if char == '(':
         stack.append(char)
+        temp *= 2
+
+    elif char == '[':
+        stack.append(char)
+        temp *= 3
+
     elif char == ')':
-        if stack.pop() == '(':
-            pass
-        else:
-            print('stack pop error')
+        if not stack or stack.pop() == '[':
             result = 0
             break
+        if buf[idx - 1] == '(':
+            result += temp
+        temp //= 2
+
     elif char == ']':
-        if stack.pop() == '[':
-            pass
-        else:
-            print('stack pop error')
+        if not stack or stack.pop() == '(':
             result = 0
             break
-print('finish')
+        if buf[idx - 1] == '[':
+            result += temp
+        temp //= 3
+if stack:
+    result = 0
+
+print(result)
