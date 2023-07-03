94dad015b961da4eccc0a7cd155d69dede86bc8e
khhandrea <khhandrea@naver.com>
2023-07-03 15:37:09 +0900

    feat: Refactor dfs and bfs function

--- 01269.cpp
+++ 01269.cpp
@@ -2,38 +2,53 @@
 #include <vector>
 #include <set>
 #include <stack>
+#include <queue>
 
 using namespace std;
+
+void dfs(set<short> *graph, short N, short V);
+void bfs(set<short> *graph, short N, short V);
 
 int main() {
     short N, M, V;
     short current, from, to;
-    short i, j;
+    short i;
     set<short> *graph;
-    set<short>::iterator iter;
-    vector<bool> *visited;
-    stack<short> dfs_stack;
-    bool deep_first;
 
     // Scan
     scanf("%hd %hd %hd", &N, &M, &V);
     graph = new set<short>[N];
-    visited = new vector<bool>(N);
 
-    // Initial stack
-    dfs_stack.push(V - 1);
 
-    // Make adjacent list and visited set
+    // Make bidirectional adjacent list
     for(i=0; i<M; i++) {
         scanf("%hd %hd", &from, &to);
         graph[from - 1].insert(to - 1);
         graph[to - 1].insert(from - 1);
     }
+
+    dfs(graph, N, V);
+    bfs(graph, N, V);
+
+    return 0;
+}
+
+void dfs(set<short> *graph, short N, short V) {
+    short current;
+    short i;
+    set<short>::iterator iter;
+    vector<bool> *visited;
+    stack<short> dfs_stack;
+    bool search_deeper;
+
+    // Initialize visited set and dfs stack
+    visited = new vector<bool>(N);
     for(i=0; i<N; i++) {
         visited->at(i) = false;
     }
+    dfs_stack.push(V - 1);
 
-    // DFS
+    // DFS loop    
     while (!dfs_stack.empty()) {
         current = dfs_stack.top();
         
@@ -42,21 +57,22 @@ int main() {
             printf("%d ", current + 1);
         }
 
-        deep_first = false;
+        search_deeper = false;
         for (iter=graph[current].begin(); iter!=graph[current].end(); iter++) {
             if (!visited->at(*iter)) {
                 dfs_stack.push(*iter);
-                deep_first = true;
+                search_deeper = true;
                 break;
             }
         }
-        if (!deep_first) {
+        
+        if (!search_deeper) {
             dfs_stack.pop();
         }
     }
     printf("\n");
+}
 
-    // BFS
+void bfs(set<short> *graph, short N, short V) {
 
-    return 0;
 }
