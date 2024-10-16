c22abebe2043bb3d0482669139e2a196afc63a01
khhandrea <khhandrea@naver.com>
2024-10-17 01:34:43 +0900

    Add 2024-10-17

--- baekjoon/01654.cpp
+++ baekjoon/01654.cpp
@@ -1,23 +1,40 @@
-// 01654번 랜선 자르기
-#include <cstdio>
-
-using namespace std;
+#include <iostream>
 
 int main() {
-    int K, N, lan, i;
-    int *lans;
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
 
-    // Input
-    scanf("%d %d", &K, &N);
-    lans = new int[K];
-    for (i=0; i<K; i++) {
-        scanf("%d", &lan);
-        lans[i] = lan;
+  short K;
+  int N, len, n_mid, ans;
+  int mid, r = 0, l = 1;
+  int *lens;
+
+  // Input
+  std::cin >> K >> N;
+  lens = new int[K];
+  for (short i = 0; i < K; i++) {
+    std::cin >> lens[i];
+    r = ((lens[i] > r) ? lens[i] : r);
+  }
+
+  // Parametric search
+  while ((r >= l) && (l > 0)) {
+    mid = 0.5 * l + 0.5 * r;
+    n_mid = 0;
+    for (short i = 0; i < K; i++) {
+      n_mid += lens[i] / mid;
     }
 
-    // Solve
+    if (n_mid >= N) {
+      ans = mid;
+      l = mid + 1;
+    }
+    else {
+      r = mid - 1;
+    }
+  }
 
-    // Output
+  std::cout << ans << '\n';
 
-    return 0;
+  return 0;
 }
